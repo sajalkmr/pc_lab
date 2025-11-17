5c763bf38f48b3b899a386b965fa41adfb17913b
sajal <sajal.kumar2003@gmail.com>
2025-11-17 23:19:57 +0530

    Implement basic MPI send/receive example

---
+++ prog5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size, number;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (size < 2) {
+        if (rank == 0) printf("Need at least 2 processes.\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    if (rank == 0) {
+        number = 42;
+        printf("Process 0 sending %d to Process 1\n", number);
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (rank == 1) {
+        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received %d from Process 0\n", number);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
